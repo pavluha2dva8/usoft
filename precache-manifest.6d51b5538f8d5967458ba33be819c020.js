3c9e0aa7ce0a1d8fad3169b5714c19f1f22a3f03
unknown <pavluha2dva8@gmail.com>
2020-12-10 13:58:19 +0200

    Updates

---
+++ precache-manifest.6d51b5538f8d5967458ba33be819c020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2131c2837d9d3ab7b7f193b65fd4b1c8",
+    "url": "/usoft/index.html"
+  },
+  {
+    "revision": "bb52f9d44e25603e27a8",
+    "url": "/usoft/static/css/main.9df212a3.chunk.css"
+  },
+  {
+    "revision": "d16fc845676d1dffd508",
+    "url": "/usoft/static/js/2.1b10fa0e.chunk.js"
+  },
+  {
+    "revision": "d163625bb83ec0f7dfacaffdda6f0922",
+    "url": "/usoft/static/js/2.1b10fa0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb52f9d44e25603e27a8",
+    "url": "/usoft/static/js/main.eb0c201a.chunk.js"
+  },
+  {
+    "revision": "16a45b582eebc86abb0e",
+    "url": "/usoft/static/js/runtime-main.f50eed3d.js"
+  }
+]);
