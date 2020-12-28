3ff2d7922cd06ff4e747aa1e71c645529b9afc8c
pavluha2dva8 <pavluha2dva8@gmail.com>
2020-12-28 15:36:45 +0200

    Updates

--- precache-manifest.56da798f9ec51bddd64794c78145d0d7.js
+++ precache-manifest.56da798f9ec51bddd64794c78145d0d7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1257a1f2e3fcc00985ed1f31955a824",
+    "revision": "3d84c06ee9977226771f52240de68a8d",
     "url": "/usoft/index.html"
   },
   {
-    "revision": "84aa0bd3650b2617ff20",
-    "url": "/usoft/static/css/main.24fc727a.chunk.css"
+    "revision": "309ce0534e8ce7d81ce4",
+    "url": "/usoft/static/css/main.6e856174.chunk.css"
   },
   {
     "revision": "3f90f49412675575db9e",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/usoft/static/js/2.ee72c1a5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "84aa0bd3650b2617ff20",
+    "revision": "309ce0534e8ce7d81ce4",
     "url": "/usoft/static/js/main.987ac3df.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/usoft/precache-manifest.89457e69b9ad104acbffba0ec12f4ca5.js"
+  "/usoft/precache-manifest.56da798f9ec51bddd64794c78145d0d7.js"
 );
 
 self.addEventListener('message', (event) => {
